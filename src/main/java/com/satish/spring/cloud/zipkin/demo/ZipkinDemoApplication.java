4b760648150369b88df6459b4a750d164bd1eb1a
ADHARMAN\insmundru <Satish.Mundru@Harman.com>
2017-12-30 16:29:35 +0530

    initial commit

---
+++ src/main/java/com/satish/spring/cloud/zipkin/demo/ZipkinDemoApplication.java
@@ -0,0 +1,12 @@
+package com.satish.spring.cloud.zipkin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinDemoApplication.class, args);
+	}
+}
